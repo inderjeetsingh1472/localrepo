93e61789c92373b532120207c27d18ba2eb41df7
inderjeet1472 <gate37814@gmail.com>
2024-03-01 18:37:52 +0530

    this is java script

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("hello world");
+inder@1472("the best thing the best full of the find the go the best in the in the could thing in the find the best full of the best in the way to recover of rthe ")
+console.log("code is running....")
+console.log("code is also running")
+var a=prompt("enter your number")
+var isTrue = confirm("are you sure you want to leave this page and burn your computer")
+if(isTrue){
+    console.log("computer is blasting");
+}
+else{
+    console.log("computer is not blasting") 
+
+}
+console.log("your number is " + a)
+// following is used to change the title of document
+document.title="hey i am good"
+// the following code is used to add inline css in any element
+document.body.style.backgroundColor="red"
