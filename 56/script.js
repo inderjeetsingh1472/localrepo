56614ac9f7438a32d314fa18551f08e44e2c22ba
inderjeet1472 <gate37814@gmail.com>
2024-03-02 22:40:40 +0530

    if-else

---
+++ 56/script.js
@@ -0,0 +1,37 @@
+console.log("hello i am a conditional tutorial")
+
+let age=45;
+let grace =2;
+//arthimetic opertaors=
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)//this is a exponential function which give output 45^2;
+console.log(age%grace)
+
+
+//assignment operators=
+//=  +=  -=   /=  *=  %= **=;
+
+
+//comparison opertaors=
+// ==    !=   
+// === (equal to value and type)
+// !== (not equal to value or not equal type) 
+// >  <  <=  >==
+// ? (terniary operator);
+let a=5;
+let b=9;
+c=a>b ?(a-b):(b-a)//this give value (b-a) to c because a>b is false
+console.log(c)
+
+
+//logical opertaor
+// && || !
+if(age>18){
+    console.log("you can drive");
+}
+else{
+    console.log("you cannot drive");
+}
