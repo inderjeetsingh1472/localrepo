4184c2107a112fa5cff0d4f4ccfbf688cf75dd0f
inderjeet1472 <gate37814@gmail.com>
2024-03-03 13:07:24 +0530

    loops

--- 57/script.js
+++ 57/script.js
@@ -1,1 +1,28 @@
 console.log("hey i am on a tutorial for loop")
+let a=1;
+for (let i = 0; i < 100; i++) {
+console.log(a+i)    
+}
+let obj={
+    name :"inder",
+    role:"learner",
+    company:"coding",
+}
+for (const key in obj) {
+        const element = obj[key];
+        console.log(key,element)
+        
+    }
+    for (const char of "inder") {
+        console.log(char)
+    }
+    let i=5;
+
+    while (i<10) {
+        console.log("inder")
+        i++;
+    }
+    let m=5;
+    do {
+        console.log("harjeet")
+    } while (i<5);//it print value beforee checking condition so now where condition is false but it give one output 
