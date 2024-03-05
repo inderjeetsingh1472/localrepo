e5c97211648bebcbb08397460003cd63cd51389b
inderjeet1472 <gate37814@gmail.com>
2024-03-05 18:21:35 +0530

    string

---
+++ 60/script.js
@@ -0,0 +1,35 @@
+console.log("this is string tutorial")
+let a="inderjeet singh";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[8])
+console.log(a[9])
+console.log(a[10])
+console.log(a[11])
+console.log(a[12])
+console.log(a[13])
+console.log(a[14])
+console.log(a[15])
+
+console.log(a.length)
+
+let real_name="inder"
+let friend="jassi"
+
+console.log("his name is "+ real_name + " and his friend name is "  + friend)
+console.log(`his name is ${real_name} and his friend name is ${friend}`) //these are template literals
+let b="Harjeet";
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))//here 4 is not included;
+console.log(b.slice(2))
+console.log(b.replace("Har","098"))
+//concadinate:-
+console.log(b.concat(a))
+console.log(b,"singh")
