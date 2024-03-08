c8c6d9312931074ae86ee4b2907de73a45fe7d83
inderjeet1472 <gate37814@gmail.com>
2024-03-08 10:17:32 +0530

    events

---
+++ 74/script.js
@@ -0,0 +1,5 @@
+let button=document.getElementById("btn")
+//https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+button.addEventListener("dblclick",()=>{
+    alert("i was clicked")
+})
